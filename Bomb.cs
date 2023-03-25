8b28a5ad1695c46fa99911ab83fd57199fc1e3fd
Rishi Anand <flywithmerishi16@gmail.com>
2023-03-25 11:33:49 +0530

    first commit

---
+++ Bomb.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+  public float countDown;
+
+
+  // Update is called once per frame
+  void Update()
+  {
+    countDown -= Time.deltaTime;
+
+    if (countDown <= 0f)
+    {
+      FindObjectOfType<MapDestroyer>().Explode(transform.position);
+      Destroy(gameObject);
+    }
+
+
+  }
+}
---
+++ BombSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombSpawner : MonoBehaviour
+{
+  public Tilemap tilemap;
+  public Tile wallTile;
+
+  public GameObject bombPrefab;
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      //Debug.Log("worldPos " + worldPos);
+      Vector3Int cell = tilemap.WorldToCell(worldPos);
+      //Debug.Log("cell " + cell);
+      Vector3 cellCenterPos = tilemap.GetCellCenterWorld(cell);
+
+      Tile tile = tilemap.GetTile<Tile>(cell);
+      if (tile != wallTile)
+      {
+        Instantiate(bombPrefab, cellCenterPos, Quaternion.identity);
+      }
+
+    }
+  }
+}
